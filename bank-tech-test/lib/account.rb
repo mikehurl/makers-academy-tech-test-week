26ea348fa82059748a6f3a1a2c8104ca9a666a76
Mike Hurl <mikehurl@gmail.com>
2017-01-23 19:26:26 +0000

    prints statement in format requested. initialize method, deposit method and withdraw method had to refactored to make this work

--- bank-tech-test/lib/account.rb
+++ bank-tech-test/lib/account.rb
@@ -1,20 +1,32 @@
 class Account
 
-  attr_reader :balance, :transactions
+  attr_reader :balance, :transactions, :transaction_log
 
   def initialize
     @balance = 0
     @transactions = []
+    @transaction_history = []
   end
 
   def deposit(amount)
     @balance += amount
-    @transactions << {"deposit" => [datestamp, amount]}
+    @transaction_history << "#{datestamp} || #{amount.to_s}.00||         || #{@balance}.00\n"
+    @transactions << [datestamp, amount]
   end
 
   def withdraw(amount)
     @balance -= amount
-    @transactions << {"withdraw" => [datestamp, amount]}
+    @transaction_history << "#{datestamp} ||        || #{amount.to_s}.00  || #{@balance}.00\n"
+    @transactions << [datestamp, amount]
+  end
+
+  def print_statement
+    title_row = "date       || credit || debit   || balance\n"
+    title_row + transaction_log.join("").strip
+  end
+
+  def transaction_log
+    @transaction_history.reverse!
   end
 
   private
--- bank-tech-test/spec/account_spec.rb
+++ bank-tech-test/spec/account_spec.rb
@@ -6,7 +6,7 @@ describe Account do
     @account = Account.new
   end
 
-  let!(:time) {Time.new(2017, 01, 23)}
+  let!(:time) {Time.new(2017, 01, 23).strftime "%d/%m/%Y"}
 
   it 'starts with a balance of zero' do
     expect(@account.balance).to eq 0
@@ -21,23 +21,30 @@ describe Account do
     end
 
     it 'date-stamps the deposit transaction' do
-      expect(@account.deposit(500)).to eq [{"deposit"=>["23/01/2017", 500]}]
+      expect(@account.deposit(500)).to eq [[time, 500]]
+    end
+
+    it 'prints a statement of transactions' do
+      @account.deposit(1000)
+      @account.deposit(2000)
+      @account.withdraw(500)
+      expect(@account.print_statement).to eq "date       || credit || debit   || balance\n#{time} ||        || 500.00  || 2500.00\n#{time} || 2000.00||         || 3000.00\n#{time} || 1000.00||         || 1000.00"
     end
 
   end
 
-    describe '#withdraw' do
+  describe '#withdraw' do
 
-      it { is_expected.to respond_to(:withdraw).with(1).argument }
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
 
-      it 'can reduce the balance' do
-        expect{ @account.withdraw(500) }.to change{ @account.balance }.by -500
-      end
-
-      it 'date-stamps the withdraw transaction' do
-        expect(@account.withdraw(500)).to eq [{"withdraw"=>["23/01/2017", 500]}]
-      end
-
+    it 'can reduce the balance' do
+      expect{ @account.withdraw(500) }.to change{ @account.balance }.by -500
     end
+
+    it 'date-stamps the withdraw transaction' do
+      expect(@account.withdraw(500)).to eq [[time, 500]]
+    end
+
+  end
 
 end
