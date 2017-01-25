beef44c47838e4b159da7bafdd9731e60432de6c
Mike Hurl <mikehurl@gmail.com>
2017-01-25 13:20:12 +0000

    refactors the spec file to have a before loop to create an array of items and run the #update_quality method and removed the creation of an item and running the #update_quality method from the individual specs

--- gilded-rose-tech-test/spec/gilded_rose_spec.rb
+++ gilded-rose-tech-test/spec/gilded_rose_spec.rb
@@ -2,24 +2,23 @@ require 'gilded_rose'
 
 describe GildedRose do
 
+  before do
+    @items = [Item.new("foo", 0, 0), Item.new("test item", 1, 1), Item.new("test item", 0, 2)]
+    GildedRose.new(@items).update_quality()
+  end
+
   describe "#update_quality" do
 
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(@items[0].name).to eq "foo"
     end
 
     it 'lowers the sell_in and quality values' do
-      items = [Item.new("test item", 1, 1)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].to_s).to eq 'test item, 0, 0'
+      expect(@items[1].to_s).to eq 'test item, 0, 0'
     end
 
     it 'degrades the quality twice as fast once the sell by date has passed, which is when the sell_in value is zero' do
-      items = [Item.new("test item", 0, 2)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].to_s).to eq 'test item, -1, 0'
+      expect(@items[2].to_s).to eq 'test item, -1, 0'
     end
 
   end
