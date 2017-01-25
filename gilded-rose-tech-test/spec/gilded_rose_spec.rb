aad44a27efe11d35ed458d926427a732a5ff454f
Mike Hurl <mikehurl@gmail.com>
2017-01-25 14:13:21 +0000

    creates and passes a test to check the quality of 'Backstage passes' increases by two when the sell_in value is greater than five and less than or equal to ten

--- gilded-rose-tech-test/spec/gilded_rose_spec.rb
+++ gilded-rose-tech-test/spec/gilded_rose_spec.rb
@@ -3,7 +3,7 @@ require 'gilded_rose'
 describe GildedRose do
 
   before do
-    @items = [Item.new("foo", 0, 0), Item.new("test item", 1, 1), Item.new("test item", 0, 2), Item.new("Aged Brie", 3, 3), Item.new("Aged Brie", 1, 50), Item.new("Sulfuras, Hand of Ragnaros", 5, 5), Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 5)]
+    @items = [Item.new("foo", 0, 0), Item.new("test item", 1, 1), Item.new("test item", 0, 2), Item.new("Aged Brie", 3, 3), Item.new("Aged Brie", 1, 50), Item.new("Sulfuras, Hand of Ragnaros", 5, 5), Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 5)]
     GildedRose.new(@items).update_quality()
   end
 
@@ -39,7 +39,12 @@ describe GildedRose do
     end
 
     it 'increases the quality of "Backstage passes" by one when the sell_in value is greater than or equal to eleven' do
-      expect(@items[6].to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 11, 6'
+      expect(@items[6].to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 10, 6'
+    end
+
+    it 'increases the quality of "Backstage passes" by two when the sell_in value is greater than five and less than or equal to ten' do
+      GildedRose.new(@items).update_quality()
+      expect(@items[6].to_s).to eq 'Backstage passes to a TAFKAL80ETC concert, 9, 8'
     end
 
   end
