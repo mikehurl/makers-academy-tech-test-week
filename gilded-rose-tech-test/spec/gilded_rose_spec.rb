d17ef7ea3cc14c9f551262e02720b67f5323529f
Mike Hurl <mikehurl@gmail.com>
2017-01-25 12:05:51 +0000

    creates and passes test to check that #update-quality does lower the sell-in and quality values of the item

--- gilded-rose-tech-test/spec/gilded_rose_spec.rb
+++ gilded-rose-tech-test/spec/gilded_rose_spec.rb
@@ -3,11 +3,19 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it 'lowers the sell_in and quality values' do
+      items = [Item.new("test item", 1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].to_s).to eq 'test item, 0, 0'
+    end
+
   end
 
 end
